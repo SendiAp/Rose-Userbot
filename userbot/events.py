650cfcd35b0c38a2bb4aed0ee88aecbc3b68c3b6
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-07 22:00:48 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -96,11 +96,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Geez-Project ERROR**\n"
-                    link = "Silahkan chat: @VckyouuBitch"
+                    text = "**Sky-Project ERROR**\n"
+                    link = "Silahkan chat: @pikyus1"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
-                    text += "Vcky Siap Membantu Kamu\n"
+                    text += "Sendi Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
