01916af44b9d7f31d8a2c429b128c8febf8ef4c4
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-10 13:40:39 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -146,7 +146,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na Project by [ROSE](https://t.me/UserbotSupport1)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
