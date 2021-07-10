6682c8df98b28656d0cd34b338abf7c1dd9988b0
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-10 14:18:51 +0700

    Update rosehelper.py

--- userbot/modules/rosehelper.py
+++ userbot/modules/rosehelper.py
@@ -12,9 +12,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/VckyouuBitch)"
-        "\n[Repo](https://github.com/Vckyou/Geez-UserBot)"
-        "\n[Instagram](instagram.com/Vckyouuu)")
+        "\n[Telegram](t.me/pikyus1)"
+        "\n[Repo](https://github.com/S/Rose-UserBot)"
+        "\n[Instagram](instagram.com/ndiap04)")
 
 
 @register(outgoing=True, pattern="^.vars$")
