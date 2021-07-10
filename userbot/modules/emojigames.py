7a86d6e514c0afab7f2ecba01067455b850f29f8
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-11 00:22:34 +0700

    Update emojigames.py

--- userbot/modules/emojigames.py
+++ userbot/modules/emojigames.py
@@ -73,8 +73,24 @@ async def _(event):
         except BaseException:
             pass
 
+@register(outgoing=True, pattern="^.petir(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('⚡'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('⚡'))
+        except BaseException:
+            pass
+
 CMD_HELP.update({
     "emojigames":
-    "`.dice` 1-6 or `.dart`1-6 or `.ball`1-5 `.dadu`1-5\
+    "`.dice` 1-6 or `.dart`1-6 or `.ball`1-5 `.dadu`1-5 `.petir`1-5\
 \nUsage: hahaha just a magic.\nWarning:`Don't use any other values or bot will crash`"
 })
