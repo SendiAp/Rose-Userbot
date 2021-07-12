666e4b417d47fd7280dd769f86da7e8412905cb5
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-13 03:13:47 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -80,7 +80,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [ROSE](https://t.me/UserbotSupport1)",
+        \na project by [Rose](https://t.me/Rose_Userbot)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -146,7 +146,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na Project by [ROSE](https://t.me/UserbotSupport1)",
+        \na Project by [Rose](https://t.me/Rose_Userbot)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -212,7 +212,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Rose](https://t.me/Rose_Userbot)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -278,7 +278,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Rose](https:/t.me/Rose_Userbot)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
