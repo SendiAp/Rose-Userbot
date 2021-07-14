964d5da6b86993fa21b1e02d26bc5efd65ee822a
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-15 04:27:53 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -344,7 +344,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [ROSE](https://t.me/UserbotSupport1)",
+        \na project by [Rose](https://t.me/Rose_Userbot)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
