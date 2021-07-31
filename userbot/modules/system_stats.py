2ee1c22a1e52766a864e081f2dcc4f58acb6c43a
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-08-01 03:38:05 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -230,14 +230,14 @@ async def amireallyalive(alive):
     output = (
         f"尺ㄖ丂乇 ㄩ丂乇尺乃ㄖㄒ\n\n"
         f"\n__**{ROSE_TEKS_KUSTOM}**__\n\n\n"
-        f"╭✠╼━━━━━━━━━━━━━━━━━━━━✠╮\n"
+        f"╭✠╼━━━━━━━━━━━━━━━━━✠╮\n"
         f"◙ **Name** : {DEFAULTUSER} \n"
         f"◙ **Username** : @{user.username} \n"
         f"◙ **Telethon** : {version.__version__} \n"
         f"◙ **Python**   : {python_version()} \n"
         f"◙ **Bot Ver**  : {BOT_VER} \n"
         f"◙ **Modules**  : {len(modules)} \n"
-        f"╰✠╼━━━━━━━━━━━━━━━━━━━━✠╯\n"
+        f"╰✠╼━━━━━━━━━━━━━━━━━✠╯\n"
         f"[ɢʀᴏᴜᴘꜱ](https://t.me/Rose_Userbot) | [ʙᴏᴛᴏꜰ](https://t.me/{user.username}) | [ɢɪᴛʜᴜʙ](https://github.com/SendiAp/Rose-Userbot)")
     if ALIVE_LOGO:
         try:
