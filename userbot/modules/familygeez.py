a50f281dd9540666ed175db7446d1779c11b1619
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-08-05 04:44:45 +0700

    Update familygeez.py

--- userbot/modules/familygeez.py
+++ userbot/modules/familygeez.py
@@ -28,8 +28,33 @@ async def typewriter(typew):
     await typew.edit("**Tapi Bo'ong Hiyahiyahiya**")
 # Create by myself @localheart
 
+@register(outgoing=True, pattern='^.adudin(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**Adudin**")
+    sleep(1)
+    await typew.edit("**Adudin Ganteng Banget**")
+    sleep(1)
+    await typew.edit("**Boleh Aku minta vps? **")
+    sleep(1)
+    await typew.edit("**Kan dudin Mukanya Kaya Babi🙈**")
+    sleep(1)
+    await typew.edit("**Ehh Engga Deh,Dudin Kan Ganteng Kaya Artis Korea😄**")
+    sleep(1)
+    await typew.edit("**Tapi Boong😂**")
+    sleep(1)
+    await typew.edit("**HAHAHAHAHAHAHA**")
+    sleep(1)
+    await typew.edit("**Udah Ahh Takut Dudin Nangis Minta Balon😂**")
+    sleep(1)
+    await typew.edit("**Pokoknya Dudin Ganteng Banget**")
+    sleep(1)
+    await typew.edit("**Tapi Bo'ong Hiyahiyahiya**")
+# Create by myself @localheart
+
 CMD_HELP.update({
     "familygeez":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vicky`\
+    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vicky` / `.adudin`\
     \n↳ : Untuk Ngatain Vicky."
 })
