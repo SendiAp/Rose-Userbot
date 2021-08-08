43da2438a773d78639786213a17620e29af00701
SendiAp <SendiAp.farosbmc123@gmail.com>
2021-08-08 19:20:06 +0000

    pylint: auto fixes
    Signed-off-by: SendiAp <SendiAp.farosbmc123@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -489,11 +489,11 @@ with bot:
             if event.message.from_id != uid:
                 u = await event.client.get_entity(event.chat_id)
                 await tgbot.send_file(event.chat_id, file=logo,
-                await event.reply(
-                    f"Haii!![{get_display_name(u)}](tg: // user?id={u.id})\n\n"
-                    f"**🌹 Saya Adalah Rose - Userbot **\n\n"
-                    f"`Saya Adalah Userbot Yang Dipakai User Telegram, Jika Kamu Mau Seperti {DEFAULTUSER} Masuk Grub Kami Untuk Info lebih lanjut.`\n\n"
-                    f"🤴 **ʙᴏᴛᴏꜰ :** {DEFAULTUSER}\n📓 **ᴍᴏᴅᴜʟᴇꜱ :** {len(plugins)}\n⚙ **ʜᴀɴᴅʟᴇʀꜱ :** Titik\n📗 **ᴄᴏᴍᴀɴᴅ :** /ping - /setting")
+                                      await event.reply(
+                                          f"Haii!![{get_display_name(u)}](tg: // user?id={u.id})\n\n"
+                                          f"**🌹 Saya Adalah Rose - Userbot **\n\n"
+                                          f"`Saya Adalah Userbot Yang Dipakai User Telegram, Jika Kamu Mau Seperti {DEFAULTUSER} Masuk Grub Kami Untuk Info lebih lanjut.`\n\n"
+                                          f"🤴 **ʙᴏᴛᴏꜰ :** {DEFAULTUSER}\n📓 **ᴍᴏᴅᴜʟᴇꜱ :** {len(plugins)}\n⚙ **ʜᴀɴᴅʟᴇʀꜱ :** Titik\n📗 **ᴄᴏᴍᴀɴᴅ :** /ping - /setting")
                                       buttons=[
                                           [custom.Button.url(text="➕ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕",
                                            url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
