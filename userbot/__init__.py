c1708c7e9994587138e3121e5619dda482852938
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-08-09 01:13:18 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -603,7 +603,7 @@ with bot:
                 reply_pop_up_alert = f"❌ WARNINGS ❌\n\nAnda Tidak Mempunyai Hak Untuk Menekan Tombol Button Ini."
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-       @tgbot.on(
+         @tgbot.on(
              events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                  data=re.compile(rb"nepo")
             )
