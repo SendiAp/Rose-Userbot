2bbc4ca9c20eb3b399e3e8243a27745de6abfe0c
SendiAp <SendiAp.farosbmc123@gmail.com>
2021-08-08 18:20:24 +0000

    pylint: auto fixes
    Signed-off-by: SendiAp <SendiAp.farosbmc123@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -604,8 +604,8 @@ with bot:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @tgbot.on(
-             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-                 data=re.compile(rb"nepo")
+            events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+                data=re.compile(rb"nepo")
             )
         )
         async def on_plug_in_callback_query_handler(event):
