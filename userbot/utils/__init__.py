448005dc3932c25ca4707025beaa916c64d91698
SendiAp <SendiAp.farosbmc123@gmail.com>
2021-08-09 13:55:10 +0000

    pylint: auto fixes
    Signed-off-by: SendiAp <SendiAp.farosbmc123@gmail.com>

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -18,13 +18,13 @@ from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
 from .tools import (
-    humanbytes, 
-    time_formatter, 
-    human_to_bytes, 
-    run_cmd, md5, 
-    post_to_telegraph, 
-    take_screen_shot, 
-    runcmd, 
+    humanbytes,
+    time_formatter,
+    human_to_bytes,
+    run_cmd, md5,
+    post_to_telegraph,
+    take_screen_shot,
+    runcmd,
     edit_delete,
     edit_or_reply,
-) 
+)
