867d9ec9cf33d68b1b5dc2bd74a1a28f4aab97bb
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-08-11 03:23:10 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,7 +25,7 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("`{argh}`**Plugin Tidak Valid**")
+            await event.edit("`{args}` **Plugin Tidak Valid**")
             await asyncio.sleep(200)
             await event.delete()
     else:
