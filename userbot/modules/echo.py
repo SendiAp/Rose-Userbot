1ee824b28650e9860dae729a2debc454f95727cb
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-08-10 11:01:28 +0700

    Update echo.py

--- userbot/modules/echo.py
+++ userbot/modules/echo.py
@@ -14,6 +14,7 @@ from userbot.modules.sql_helper.echo_sql import (
     remove_echo,
     remove_echos,
 )
+from userbot.utils.event import get_user_from_event
 
 
 @register(outgoing=True, pattern=r"^\.addecho(?: |$)(.*)")
