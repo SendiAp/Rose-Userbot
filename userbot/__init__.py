c290a01e75618bd2152fd33453fb65b97d6a6ad2
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-08-11 15:31:23 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -176,8 +176,8 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 
-# Untuk Perintah .geez
-GEEZ_TEKS_KUSTOM = os.environ.get("GEEZ_TEKS_KUSTOM", None)
+# Untuk Perintah .rosealive
+ROSE_TEKS_KUSTOM = os.environ.get("ROSE_TEKS_KUSTOM", None)
 
 # Default .alive Name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
