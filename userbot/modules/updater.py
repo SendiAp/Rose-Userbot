54734d2eb131ae44d19a8db899d128194cdfd840
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-16 20:35:26 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -53,7 +53,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await event.edit(
                 '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
-                ' untuk dapat deploy perubahan terbaru dari ⚡Sƙყȥυ-υႦσƚ⚡.`'
+                ' untuk dapat deploy perubahan terbaru dari ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡.`'
             )
             repo.__del__()
             return
@@ -115,9 +115,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**⚡Sƙყȥυ-υႦσƚ⚡** `Berhasil Di Update!`')
+    await event.edit('**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡** `Berhasil Di Update!`')
     await asyncio.sleep(1)
-    await event.edit('**⚡Sƙყȥυ-υႦσƚ⚡** `Di Restart....`')
+    await event.edit('**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡** `Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Mohon Menunggu Beberapa Detik.`')
     await asyncio.sleep(10)
@@ -126,7 +126,7 @@ async def update(event, repo, ups_rem, ac_br):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID, "#BOT \n"
-            "**⚡Sƙყȥυ-υႦσƚ⚡ Telah Di Perbarui.**")
+            "**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡ Telah Di Perbarui.**")
         await asyncio.sleep(100)
         await event.delete()
 
@@ -188,13 +188,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n⚡Sƙყȥυ-υႦσƚ⚡ Sudah Versi Terbaru || Tunggu Update Terbaru\n')
+            f'\n⚡Sƙყȥυ-υʂҽɾႦσƚ⚡ Sudah Versi Terbaru || Tunggu Update Terbaru\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk ⚡Sƙყȥυ-υႦσƚ⚡ :\n\n⚒️ Pembaruan Data :**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡ :\n\n⚒️ Pembaruan Data :**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
@@ -214,12 +214,12 @@ async def upstream(event):
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('` Proses Update ⚡Sƙყȥυ-υႦσƚ⚡, Loading....1%`')
-        await event.edit('` Proses Update ⚡Sƙყȥυ-υႦσƚ⚡ Loading....20%`')
-        await event.edit('` Proses Update ⚡Sƙყȥυ-υႦσƚ⚡, Loading....35%`')
-        await event.edit('` Proses Update ⚡Sƙყȥυ-υႦσƚ⚡, Loading....77%`')
-        await event.edit('` Proses Update ⚡Sƙყȥυ-υႦσƚ⚡, Updating...90%`')
-        await event.edit('` Proses Update ⚡Sƙყȥυ-υႦσƚ⚡, Mohon Tunggu Sebentar....100%`')
+        await event.edit('` Proses Update ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡, Loading....1%`')
+        await event.edit('` Proses Update ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡ Loading....20%`')
+        await event.edit('` Proses Update ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡, Loading....35%`')
+        await event.edit('` Proses Update ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡, Loading....77%`')
+        await event.edit('` Proses Update ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡, Updating...90%`')
+        await event.edit('` Proses Update ⚡Sƙყȥυ-υʂҽɾႦσƚ⚡, Mohon Tunggu Sebentar....100%`')
 
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
