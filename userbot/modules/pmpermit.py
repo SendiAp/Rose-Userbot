de00f0a8b6e1c45aa24222d98560b5e1a1020776
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-16 20:13:27 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -44,11 +44,11 @@ DEF_UNAPPROVED_MSG = (
     "╔═════════════════════╗\n"
     "   Ⓦ︎Ⓔ︎Ⓛ︎Ⓒ︎Ⓞ︎Ⓜ︎Ⓔ︎ Ⓟ︎Ⓔ︎Ⓢ︎Ⓐ︎Ⓝ︎    ”\n"
     "╚═════════════════════╝\n"
-    "**TOLONG JANGAN MELAKUKAN SPAM CHAT KEPADA BOS SAYA** \n"
+    "**TOLONG JANGAN MELAKUKAN SPAM CHAT KEPADA MAJIKAN SAYA** \n"
     f"**KARENA SAYA AKAN OTOMATIS MEMBLOKIR KAMU, TUNGGU SAMPAI {DEFAULTUSER} MENERIMA PESAN KAMU** \n"
     "╔═════════════════════╗\n"
     "│○›ᑭᗴՏᗩᑎ OTOᗰᗩTIՏ           \n"
-    f"│○›ᗷY ᖇOՏᗴ-ᑌՏᗴᖇᗷOT           \n"
+    f"│○›ᗷY Sƙყȥυ-υႦσƚ           \n"
     "╚═════════════════════╝")
 # =================================================================
 
