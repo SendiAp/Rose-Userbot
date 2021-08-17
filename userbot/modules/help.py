2b7e3825ca53333a598ef0b93a6aea8285f8b1a7
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-17 10:36:11 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,7 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ❉  "
-        await event.edit("**🌹འօʂҽ-Աʂҽɾҍօէ🌹**\n\n"
+        await event.edit("**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
                          f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`")
