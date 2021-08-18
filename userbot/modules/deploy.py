3d7a100736122de239260b5993b79af67def9dfc
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-18 14:26:23 +0700

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -46,7 +46,7 @@ async def _(event):
         "INFO - Rose-Userbot: Logged in as 557667062",
         "INFO - Rose-Userbot: Successfully...",
         "919852+00:00 app[worker.1]: 919 - Rose-Userbot -",
-        "INFO - 🌹 Rose-Userbot 🌹 ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
+        "INFO - ⚡ Skyzu-Userbot ⚡ ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**"
 
     ]
