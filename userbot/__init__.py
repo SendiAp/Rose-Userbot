3766d89d7e0b51ce466425bd2baebe5edd14473c
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-19 18:19:29 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -111,7 +111,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Askarbot/Skyzu-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Skyzu-Userbot")
+    "UPSTREAM_REPO_BRANCH", "Rose-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
