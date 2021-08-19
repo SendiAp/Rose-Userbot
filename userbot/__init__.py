20a5d6aaffa82959ee8ef1e03656f45b4a210f4a
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-19 08:05:00 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -176,7 +176,7 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 
-# Untuk Perintah .skyzualive
+# Untuk Perintah .rosealive
 ROSE_TEKS_KUSTOM = os.environ.get("ROSE_TEKS_KUSTOM", None)
 
 # Untuk Mengubah Pesan Welcome
