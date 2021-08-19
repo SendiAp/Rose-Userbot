0da23ba8799c9a7093ab2b2bf34bc302453ade1f
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-08-19 09:25:56 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -92,7 +92,7 @@ async def redis(pong):
     await pong.edit("__Sedang Memuat...__")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**⚡Skyzu-Userbot⚡**\n"
+    await pong.edit(f"**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡**\n"
                     f"➾ __Signal__    __:__ "
                     f"`%sms` \n"
                     f"➾ __Uptime__ __:__ "
