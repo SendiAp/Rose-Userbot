82db6ff9907ab94b152b7556d1affa380399c0dd
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2021-12-21 19:57:29 +0700

    Create decorators.py

---
+++ userbot/utils/decorators.py
@@ -0,0 +1,68 @@
+import inspect
+import re
+from pathlib import Path
+
+from telethon import events
+
+from userbot import (
+    CMD_HANDLER,
+    CMD_LIST,
+    SUDO_HANDLER,
+    SUDO_USERS,
+    bot,
+)
+
+
+def rose_cmd(
+    pattern: str = None,
+    allow_sudo: bool = True,
+    disable_edited: bool = False,
+    forword=False,
+    command: str = None,
+    **args,
+):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    args["blacklist_chats"] = True
+    black_list_chats = list(BL_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    if pattern is not None:
+        global rose_reg
+        global sudo_reg
+        if (
+            pattern.startswith(r"\#")
+            or not pattern.startswith(r"\#")
+            and pattern.startswith(r"^")
+        ):
+            man_reg = sudo_reg = re.compile(pattern)
+        else:
+            man_ = "\\" + CMD_HANDLER
+            sudo_ = "\\" + SUDO_HANDLER
+            man_reg = re.compile(man_ + pattern)
+            sudo_reg = re.compile(sudo_ + pattern)
+            if command is not None:
+                cmd1 = rose_ + command
+                cmd2 = sudo_ + command
+            else:
+                cmd1 = (
+                    (rose_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+                cmd2 = (
+                    (sudo_ + pattern)
+                    .replace("$", "")
+                    .replace("\\", "")
+                    .replace("^", "")
+                )
+            try:
+                CMD_LIST[file_test].append(cmd1)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd1]})
