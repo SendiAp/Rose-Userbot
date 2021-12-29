4efe3f535d247baa5bffe9d8325aaf7cf5953f71
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2021-12-29 11:01:41 +0700

    Update xiaomi.py
    • Change the language to Indonesian.
    • Improve Text Edit When Using Commands.
    • Changed CMD_HELP to be nicer and legible.
    • Fixes Modules.

--- userbot/modules/xiaomi.py
+++ userbot/modules/xiaomi.py
@@ -2,7 +2,7 @@
 
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+from userbot import bot, CMD_HELP, ALIVE_NAME
 from userbot.events import register
 
 
@@ -12,7 +12,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     firmware = "firmware"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -22,7 +22,7 @@ async def _(event):
             await conv.send_message(f'/{firmware} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -35,7 +35,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     fboot = "fastboot"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -45,7 +45,7 @@ async def _(event):
             await conv.send_message(f'/{fboot} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBoot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -58,7 +58,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     recovery = "recovery"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -68,7 +68,7 @@ async def _(event):
             await conv.send_message(f'/{recovery} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -81,7 +81,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     pitch = "pb"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -91,7 +91,7 @@ async def _(event):
             await conv.send_message(f'/{pitch} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -104,7 +104,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     ofox = "of"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -114,7 +114,7 @@ async def _(event):
             await conv.send_message(f'/{ofox} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -127,7 +127,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     eu = "eu"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -137,7 +137,7 @@ async def _(event):
             await conv.send_message(f'/{eu} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -150,7 +150,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     vendor = "vendor"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -160,7 +160,7 @@ async def _(event):
             await conv.send_message(f'/{vendor} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
@@ -173,7 +173,7 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     specs = "specs"
-    await event.edit("```Processing```")
+    await event.edit("`Memproses...`")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
         try:
             response = conv.wait_event(
@@ -183,29 +183,32 @@ async def _(event):
             await conv.send_message(f'/{specs} {link}')
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Unblock @XiaomiGeeksBot plox```")
+            await event.reply("✖️ `Maaf {ALIVE_NAME} Kamu Sudah Menghapus Bot @XiaomiGeeksBot , Buka Kembali Bot Nya !`")
             return
         else:
             await event.delete()
             await bot.forward_messages(event.chat_id, response.message)
 
 
-CMD_HELP.update({
-    "xiaomi":
-    "For Xiaomeme devices only!\
-\n\n`.firmware` (codename)\
-     \nUsage : Get lastest Firmware\
-\n\n`.pb` (codename)\
-     \nUsage : Get latest PitchBlack Recovery\
-\n\n`.specs` (codename)\
-     \nUsage : Get quick spec information about device\
-\n\n`.fastboot` (codename)\
-     \nUsage : Get latest fastboot MIUI\
-\n\n`.recovery` (codename)\
-     \nUsage : Get latest recovery MIUI\
-\n\n`.eu` (codename)\
-    \nUsage: Get latest xiaomi.eu rom\
-\n\n`.vendor` (codename)\
-    \nUsage: fetches latest vendor\
-\n\n`.of` (codename)\
-     \nUsage : Get latest ORangeFox Recovery"})
+CMD_HELP.update(
+    {
+        "xiaomi": "**✘ Plugin** `xiaomi` :\
+        \n\n  •  **Perintah :** `.firmware` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan Firmware terbaru.\
+        \n\n  •  **Perintah :** `.pb` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan Pemulihan PitchBlack terbaru.\
+        \n\n  •  **Perintah :** `.specs` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan informasi spesifikasi cepat tentang perangkat.\
+        \n\n  •  **Perintah :** `.fastboot` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan MIUI fastboot terbaru.\
+        \n\n  •  **Perintah :** `.recovery` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan MIUI pemulihan terbaru.\
+        \n\n  •  **Perintah :** `.eu` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan rom xiaomi.eu terbaru.\        
+        \n\n  •  **Perintah :** `.vendor` [**Nama Kode**]\
+        \n  •  **Fungsi : **Mengambil Vendor Terbaru.\
+        \n\n  •  **Perintah :** `.of` [**Nama Kode**]\
+        \n  •  **Fungsi : **Dapatkan Pemulihan ORangeFox terbaru.\
+    "
+    }
+)
