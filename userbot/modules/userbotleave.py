936d60aab9402d4e85124baf12931157f2189a71
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-22 03:29:57 +0700

    Update userbotleave.py

--- userbot/modules/userbotleave.py
+++ userbot/modules/userbotleave.py
@@ -2,7 +2,7 @@ import asyncio
 
 from userbot import SUDO_USERS
 from userbot.events import register
-from userbot.utils.leavell import client as USER
+from userbot.utils import client as USER
 
 
 @register(outgoing=True, pattern=r"^.leavell(?: |$)(.*)")
