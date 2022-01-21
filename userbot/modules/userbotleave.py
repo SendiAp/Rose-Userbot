0196dee3eb54a780a67e2745571a05109e102335
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-22 03:12:46 +0700

    Update userbotleave.py

--- userbot/modules/userbotleave.py
+++ userbot/modules/userbotleave.py
@@ -4,12 +4,12 @@ from userbot import SUDO_USERS
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.leavell$")
+@register(outgoing=True, pattern=r"^.leavell(?: |$)(.*)")
 async def leavell(event):
     if event.from_user.id in SUDO_USERS:
         left = 0
         failed = 0
-        await event.edit("Asisten Meninggalkan semua obrolan")
+        await event.edit("Meninggalkan semua obrolan...")
         async for dialog in USER.iter_dialogs():
             try:
                 await USER.leave_chat(dialog.chat.id)
