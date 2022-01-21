6df9e57305eea9177cdb5d39c571e62b8f0966df
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-22 02:42:48 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -199,7 +199,7 @@ ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
 BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 
 # Bot Name
-TERM_ALIAS = os.environ.get("TERM_ALIAS", "Rose-Userbot"
+TERM_ALIAS = os.environ.get("TERM_ALIAS", "Rose-Userboot") 
 
 # Bot Version
 BOT_VER=os.environ.get("BOT_VER", "5.0")
