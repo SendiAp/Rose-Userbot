2df4d102675cb690653943c19e12299283e2893d
sendiadt <sendiadt.farosbmc123@gmail.com>
2022-01-21 19:21:58 +0000

    pylint: auto fixes
    Signed-off-by: sendiadt <sendiadt.farosbmc123@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -202,7 +202,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "Rose-Userbot"
 
 # sudousers
-ID_USERS = os.environ.get("ID_USERS", None)
+ID_USERS=os.environ.get("ID_USERS", None)
 
 # Bot Version
 BOT_VER=os.environ.get("BOT_VER", "5.0")
