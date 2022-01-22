199639b292b54556308f04d0ff13b147d44a7402
sendiadt <sendiadt.farosbmc123@gmail.com>
2022-01-22 16:13:51 +0000

    pylint: auto fixes
    Signed-off-by: sendiadt <sendiadt.farosbmc123@gmail.com>

--- userbot/modules/info.py
+++ userbot/modules/info.py
@@ -14,8 +14,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
-from userbot import cmd
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, CMD_HANDLER, bot
+from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import rose_cmd
 
 
