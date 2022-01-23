f69306b5e026c3c9c2bc1dbe377353954ce4f836
sendiadt <sendiadt.farosbmc123@gmail.com>
2022-01-23 15:26:58 +0000

    pylint: auto fixes
    Signed-off-by: sendiadt <sendiadt.farosbmc123@gmail.com>

--- userbot/modules/youtube_downloader.py
+++ userbot/modules/youtube_downloader.py
@@ -11,9 +11,9 @@ from userbot.events import rose_cmd
 
 @bot.on(rose_cmd(outgoing=True, pattern="yt(a|v|sa|sv) (.*)", disable_errors=True)
 async def download_from_youtube_(event):
-    opt = event.pattern_match.group(1).lower()
+    opt=event.pattern_match.group(1).lower()
     if opt == "a":
-        ytd = YoutubeDL(
+        ytd=YoutubeDL(
             {
                 "format": "bestaudio",
                 "writethumbnail": True,
@@ -23,16 +23,16 @@ async def download_from_youtube_(event):
                 "outtmpl": "%(id)s.mp3",
             }
         )
-        url = event.pattern_match.group(2).lower()
+        url=event.pattern_match.group(2).lower()
         if not url:
             return await event.edit("✋ `Beri saya URL (youtube) untuk mengunduh audio Dari URL kamu!`")
         try:
             request.get(url)
         except BaseException:
             return await event.edit("📂 `Berikan Tautan Audio Langsung Untuk Mengunduh..`")
-        xx = await event.edit(get_string("com_1"))
+        xx=await event.edit(get_string("com_1"))
     elif opt == "v":
-        ytd = YoutubeDL(
+        ytd=YoutubeDL(
             {
                 "format": "best",
                 "writethumbnail": True,
@@ -42,16 +42,16 @@ async def download_from_youtube_(event):
                 "outtmpl": "%(id)s.mp4",
             }
         )
-        url = event.pattern_match.group(2).lower()
+        url=event.pattern_match.group(2).lower()
         if not url:
             return await event.edit("✋ `Beri saya URL (youtube) untuk mengunduh video dari URL kamu!`")
         try:
             request.get(url)
         except BaseException:
             return await event.edit("📂 `Berikan Tautan Video Langsung Untuk Mengunduh...`")
-        xx = await event.edit(get_string("com_1"))
+        xx=await event.edit(get_string("com_1"))
     elif opt == "sa":
-        ytd = YoutubeDL(
+        ytd=YoutubeDL(
             {
                 "format": "bestaudio",
                 "writethumbnail": True,
@@ -62,15 +62,15 @@ async def download_from_youtube_(event):
             }
         )
         try:
-            query = event.text.split(" ", 1)[1]
+            query=event.text.split(" ", 1)[1]
         except IndexError:
             return await event.edit("✋ `Beri saya kueri penelusuran (youtube) untuk mengunduh audio dari URL Kamu!`"
                                     )
-        xx = await event.edit("🔍`Mencari di YouTube...`")
-        url = await get_yt_link(query)
+        xx=await event.edit("🔍`Mencari di YouTube...`")
+        url=await get_yt_link(query)
         await xx.edit("💨 `Mengunduh lagu audio...`")
     elif opt == "sv":
-        ytd = YoutubeDL(
+        ytd=YoutubeDL(
             {
                 "format": "best",
                 "writethumbnail": True,
@@ -81,12 +81,12 @@ async def download_from_youtube_(event):
             }
         )
         try:
-            query = event.text.split(" ", 1)[1]
+            query=event.text.split(" ", 1)[1]
         except IndexError:
             return await event.edit("Beri saya permintaan pencarian (youtube) untuk mengunduh video dari Pencarian kamu!"
                                     )
-        xx = await event.edit("🔍`Mencari di YouTube...`")
-        url = await get_yt_link(query)
+        xx=await event.edit("🔍`Mencari di YouTube...`")
+        url=await get_yt_link(query)
         await xx.edit("💨 `Mengunduh videi audio...`")
     else:
         return
