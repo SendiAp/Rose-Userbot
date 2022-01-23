9e98f39b0c09531984cd18164ee5ec45bf2a3150
sendiadt <sendiadt.farosbmc123@gmail.com>
2022-01-23 11:06:35 +0000

    pylint: auto fixes
    Signed-off-by: sendiadt <sendiadt.farosbmc123@gmail.com>

--- userbot/modules/zipfile.py
+++ userbot/modules/zipfile.py
@@ -6,7 +6,6 @@ from datetime import date
 import time
 import os
 
-from userbot import CMD_HANDLER as babi
 from userbot import TEMP_DOWNLOAD_DIRECTORY, ZIP_DOWNLOAD_DIRECTORY, bot, CMD_HELP
 from userbot.utils import progress
 from userbot.events import rose_cmd
