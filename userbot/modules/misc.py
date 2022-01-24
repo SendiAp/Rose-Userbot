f34797668c2efa73f67b1ba65b667d897a6855c5
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-24 21:50:03 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -19,7 +19,7 @@ from PIL import Image
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 REPOLINK = str(
-    UPSTREAM_REPO_URL) if UPSTREAM_REPO_URL else "https://github.com/vckyou/Geez-UserBot"
+    UPSTREAM_REPO_URL) if UPSTREAM_REPO_URL else "https://github.com/SendiAp/Rose-Userbot"
 # ============================================
 
 opener = urllib.request.build_opener()
