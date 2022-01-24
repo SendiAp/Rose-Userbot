8ee6e41f99b0d354559553e44b804c0489549bbf
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-24 10:49:29 +0700

    Update waifu.py

--- userbot/modules/waifu.py
+++ userbot/modules/waifu.py
@@ -8,7 +8,8 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 import os
 
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
-from userbot.events import register
+from userbot.events import rose_cmd
+from userbot import CMD_HANDLER as cmd
 
 
 EMOJI_PATTERN = re.compile(
@@ -32,7 +33,7 @@ def deEmojify(inputString: str) -> str:
     return re.sub(EMOJI_PATTERN, '', inputString)
 
 
-@register(outgoing=True, pattern=r'^.waifu(:? |$)(.*)?')
+@bot.on(rose_cmd(outgoing=True, pattern=r"waifu(?: |$)(.*)"))
 async def waifu(animu):
     #"""Generate random waifu sticker with the text!"""
 
@@ -41,7 +42,7 @@ async def waifu(animu):
         if animu.is_reply:
             text = (await animu.get_reply_message()).message
         else:
-            await animu.answer("`No text given, hence the waifu ran away.`")
+            await animu.answer("✖️ `Tidak ada teks yang diberikan, maka waifu melarikan diri.`")
             return
     animus = [15, 30, 32, 33, 40, 41, 42, 48, 55, 58]
     sticcers = await bot.inline_query(
@@ -55,27 +56,27 @@ async def waifu(animu):
         )
     except Exception:
         return await animu.edit(
-            "`You cannot send inline results in this chat (caused by SendInlineBotResultRequest)`"
+            "📛 `Anda tidak dapat mengirim hasil sebaris dalam obrolan ini (caused by SendInlineBotResultRequest)`"
         )
     await sleep(5)
     await animu.delete()
 
 
-@register(outgoing=True, pattern=r'^.hz(:? |$)(.*)?')
+@bot.on(rose_cmd(outgoing=True, pattern=r"hz(?: |$)(.*)"))
 async def _(hazmat):
-    await hazmat.edit("`Sending information...`")
+    await hazmat.edit("🔍 `Sending information...`")
     level = hazmat.pattern_match.group(2)
     if hazmat.fwd_from:
         return
     if not hazmat.reply_to_msg_id:
-        await hazmat.edit("`WoWoWo Capt!, we are not going suit a ghost!...`")
+        await hazmat.edit("🌹`WoWoWo Capt!, kita tidak akan cocok dengan hantu!...`")
         return
     reply_message = await hazmat.get_reply_message()
     if not reply_message.media:
-        await hazmat.edit("`Word can destroy anything Capt!...`")
+        await hazmat.edit("🚧 `Kata bisa menghancurkan apapun Capt!...`")
         return
     chat = "@hazmat_suit_bot"
-    await hazmat.edit("```Suit Up Capt!, We are going to purge some virus...```")
+    await hazmat.edit("```Suit Up Capt!, ​​Kami akan membersihkan beberapa virus...```")
     message_id_to_reply = hazmat.message.reply_to_msg_id
     msg_reply = None
     async with hazmat.client.conversation(chat) as conv:
@@ -100,10 +101,10 @@ async def _(hazmat):
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await hazmat.reply("`Please unblock` @hazmat_suit_bot`...`")
+            await hazmat.reply("📛 `Silakan buka blokir` @hazmat_suit_bot`...`")
             return
         if response.text.startswith("I can't"):
-            await hazmat.edit("`Can't handle this GIF...`")
+            await hazmat.edit("✖️ `Tidak dapat menangani GIF ini...`")
             await hazmat.client.delete_messages(
                 conv.chat_id,
                 [msg.id, response.id, r.id, msg_reply.id])
@@ -130,10 +131,11 @@ async def _(hazmat):
     await hazmat.delete()
     return os.remove(downloaded_file_name)
 
+
 CMD_HELP.update({
     "waifu":
-    "`.waifu` text\
-\nUsage: for custom stickers.\
-\n\n`.hz` or `.hz [flip, x2, rotate (degree), background (number), black]`\
-\nUsage: Reply to a image / sticker to suit up!."
-})
+    f"**✘ Plugin waifu\
+\n\n  •  Perintah : `{cmd}waifu` [teks]\
+  \n  •  Fungsi : Custom Stickers.\
+\n\n  •  Perintah : `{cmd}hz` [flip, X2, rotate (degree), background (number), black]\
+  \n  •  Fungsi : Replay kefoto Atau Stiker."})
