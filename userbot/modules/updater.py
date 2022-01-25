9a21402aaf0905e545c79e41127cbff0114944cc
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-25 16:55:03 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -137,7 +137,7 @@ async def update(event, repo, ups_rem, ac_br):
     return
 
 
-@rose_cmd(pattern="update( now| deploy|$)"))
+@bot.on(geezbot_cmd(outgoing=True, pattern=r"update(?: |$)(now|deploy)?"))
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     await event.edit("📝 **Mengecek Pembaruan,Silakan Menunggu....**")
