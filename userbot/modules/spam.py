969c14f3d41df3361fb715b83c12d2744b70681d
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-26 10:58:23 +0700

    Update spam.py

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -6,11 +6,12 @@
 import asyncio
 from asyncio import sleep
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
-from userbot.events import register
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot import CMD_HANDLER as cmd
+from userbot.events import rose_cmd
 
 
-@register(outgoing=True, pattern="^.cspam (.*)")
+@bot.on(rose_cmd(outgoing=True, pattern="cspam (.*)"))
 async def tmeme(e):
     cspam = str(e.pattern_match.group(1))
     message = cspam.replace(" ", "")
@@ -23,7 +24,7 @@ async def tmeme(e):
             "TSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.wspam (.*)")
+@bot.on(rose_cmd(outgoing=True, pattern="wspam (.*)"))
 async def tmeme(e):
     wspam = str(e.pattern_match.group(1))
     message = wspam.split()
@@ -36,7 +37,7 @@ async def tmeme(e):
             "WSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.spam (.*)")
+@bot.on(rose_cmd(outgoing=True, pattern="spam (.*)"))
 async def spammer(e):
     counter = int(e.pattern_match.group(1).split(' ', 1)[0])
     spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
@@ -47,7 +48,7 @@ async def spammer(e):
                                     "Spam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.picspam")
+@bot.on(rose_cmd(outgoing=True, pattern="picspam (.*)"))
 async def tiny_pic_spam(e):
     message = e.text
     text = message.split()
@@ -62,7 +63,7 @@ async def tiny_pic_spam(e):
             "PicSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.delayspam (.*)")
+@bot.on(rose_cmd(outgoing=True, pattern="delayspam (.*)")))
 async def spammer(e):
     spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
     counter = int(e.pattern_match.group(1).split(' ', 2)[1])
@@ -77,17 +78,19 @@ async def spammer(e):
             "DelaySpam was executed successfully")
 
 
+
 CMD_HELP.update({
     "spam":
-    "`.cspam` <text>\
-\nUsage: Spam the text letter by letter.\
-\n\n`.spam` <count> <text>\
-\nUsage: Floods text in the chat !!\
-\n\n`.wspam` <text>\
-\nUsage: Spam the text word by word.\
-\n\n`.picspam` <count> <link to image/gif>\
-\nUsage: As if text spam was not enough !!\
-\n\n`.delayspam` <delay> <count> <text>\
-\nUsage: `.bigspam` but with custom delay.\
-\n\n\nNOTE : Spam at your own risk !!"
-})
+    f"✘ **Plugin spam** :\
+\n\n  •  **Perintah** : `{cmd}cspam`\
+  \n  •  **Fungsi** : Spam teks huruf demi huruf.\
+\n\n  •  **Perintah** : `{cmd}spam`\
+  \n  •  **Fungsi** :  Membanjiri teks dalam obrolan.\
+\n\n  •  **Perintah** : `{cmd}wspam`\
+  \n  •  **Fungsi** :  Seolah-olah spam teks tidak cukup.\
+\n\n  •  **Perintah** : `{cmd}bigspam`\
+  \n  •  **Fungsi** :  Spam teks kata demi kata.\
+\n\n  •  **Perintah** : `{cmd}delayspam`\
+  \n  •  **Fungsi** :  tetapi dengan penundaan khusus.\
+\n\n  •  **Perintah** : `{cmd}picspam` \
+  \n  •  **Fungsi"* : Spam dengan risiko Anda sendiri"})
