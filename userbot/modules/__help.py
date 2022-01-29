9e2e5e0f5650ab220c9e6a930c18943259908aee
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-29 19:24:18 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -23,16 +23,41 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@RoseUserbot")
+            results = await event.client.inline_query(BOT_USERNAME, "@RoseUserbot")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
-        else:
-            await event.edit(
-                "`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username dengan benar. Modul telah dihentikan.`"
+        except noinline:
+            event = await event.edit(
+                "**Inline Mode Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
             )
-    except Exception:
-        return await event.edit(
-            "`Anda tidak dapat mengirim hasil sebaris dalam obrolan ini (disebabkan oleh SendInlineBotResultRequest)`"
+            async with bot.conversation("@BotFather") as conv:
+                try:
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(BOT_USERNAME)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message("Search")
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    await event.client(UnblockRequest(chat))
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(BOT_USERNAME)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message("Search")
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                await event.edit(
+                    f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}helpme` **lagi untuk membuka menu bantuan.**"
+                )
+            await bot.delete_messages(
+                conv.chat_id,
+                [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
+            )
+    else:
+        await event.edit(
+            "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
         )
