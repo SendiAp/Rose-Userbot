daf2c366327d389bc4cdf5d0f88e9402ab47134f
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-30 00:29:11 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -23,7 +23,7 @@ async def yardim(event):
     if tgbotusername is not None:
         chat = "@Botfather"
         try:
-            results = await event.client.inline_query(tgbotusername, "@RoseUserbot")
+            results = await event.inline_query(tgbotusername, "@RoseUserbot")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
