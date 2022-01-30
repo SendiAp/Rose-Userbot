944a1077355634e4fc5442f57d3d60156c4d6051
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-30 15:28:43 +0700

    Update botcontrol.py

--- userbot/modules/botcontrol.py
+++ userbot/modules/botcontrol.py
@@ -19,7 +19,6 @@ from userbot import (
     SUDO_HANDLER,
     StartTime,
     tgbot,
-    user,
 )
 from userbot.modules.sql_helper.bot_blacklists import check_is_black_list
 from userbot.modules.sql_helper.bot_starters import (
