bab1e934e97148e6e02f3d5259d3c82a44c159dd
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-30 23:18:13 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from .startup import startupmessage
+from .startup import startupmessage, add_bot_to_logger_group
 from .format import parse_pre
 from . import format as _format
 from .chrome import chrome, options
