5bb4a677383195f345742c872dbdd5a09234c1b9
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-30 22:04:29 +0700

    Create startup.py

---
+++ userbot/utils/startup.py
@@ -0,0 +1,57 @@
+import glob
+import os
+import sys
+from asyncio.exceptions import CancelledError
+from datetime import timedelta
+from pathlib import Path
+
+import requests
+from telethon import Button, functions, types, utils
+
+from userbot import BOTLOG, BOTLOG_CHATID, tgbot, bot
+from ..Config import Config
+from userbot import CMD_HANDLER as cmd
+
+from ..sql_helper.globals import gvarstatus
+
+
+
+async def startupmessage():
+    """
+    Start up message in telegram logger group
+    """
+    try:
+        if BOTLOG:
+            await tgbot.send_file(
+                BOTLOG_CHATID,
+                "https://telegra.ph/file/248b4cd5adb27bf33f15c.jpg",
+                caption="**Your Dragons-Userbot has been started successfully**",
+                buttons=[(Button.url("Support", "https://t.me/KingUserbotSupport"),)],
+            )
+    except Exception as e:
+        LOGS.error(e)
+        return None
+    try:
+        msg_details = list(get_item_collectionlist("restart_update"))
+        if msg_details:
+            msg_details = msg_details[0]
+    except Exception as e:
+        LOGS.error(e)
+        return None
+    try:
+        if msg_details:
+            await tgbot.check_testcases()
+            message = await tgbot.get_messages(msg_details[0], ids=msg_details[1])
+            text = message.text + "\n\n**Ok Bot is Back and Alive.**"
+            await drgub.edit_message(msg_details[0], msg_details[1], text)
+            if gvarstatus("restartupdate") is not None:
+                await drgub.send_message(
+                    msg_details[0],
+                    f"{cmd}ping",
+                    reply_to=msg_details[1],
+                    schedule=timedelta(seconds=10),
+                )
+            del_keyword_collectionlist("restart_update")
+    except Exception as e:
+        LOGS.error(e)
+        return None
