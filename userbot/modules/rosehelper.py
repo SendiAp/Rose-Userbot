d601c2a97fd154d8daa6d11c9efcebbf1417626c
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-01-31 14:41:04 +0700

    Update rosehelper.py

--- userbot/modules/rosehelper.py
+++ userbot/modules/rosehelper.py
@@ -1,14 +1,14 @@
 """ Userbot module for other small commands. """
-from userbot import CMD_HELP, ALIVE_NAME
-from userbot.events import register
-
+from userbot import CMD_HELP, ALIVE_NAME, bot
+from userbot.events import rose_cmd
+from userbot import CMD_HANDLER as cmd
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.lhelp$")
+@bot.on(rose_cmd(outgoing=True, pattern=r"rhelp(?: |$)(.*)"))
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
@@ -17,17 +17,18 @@ async def usit(e):
         "\n[Instagram](instagram.com/ndiap04)")
 
 
-@register(outgoing=True, pattern="^.vars$")
+@bot.on(rose_cmd(outgoing=True, pattern=r"vars(?: |$)(.*)"))
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
         "\n[DAFTAR VARS](https://raw.githubusercontent.com/SendiAp/Rose-Userbot/Rose-Userbot/varshelper.txt)")
 
 
+
 CMD_HELP.update({
     "helper":
-    "`.lhelp`\
-\nUsage: Bantuan Untuk Rose-Userbot.\
-\n`.vars`\
-\nUsage: Melihat Daftar Vars."
-})
+    f"✘ Plugin helper :\
+\n\n  •  Perintah : `{cmd}vars` \
+  \n  •  Fungsi : Melihat Daftar Vars.\
+\n\n  •  Perintah : `{cmd}rhelp` \
+  \n  •  Fungsi : Bantuan Untuk Rose-Userbot."})
