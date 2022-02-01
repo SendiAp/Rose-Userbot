02eefbcc2676972f2f9548ae94dd44e6489d9f67
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-01 11:12:55 +0700

    Update botcontrol.py

--- userbot/modules/botcontrol.py
+++ userbot/modules/botcontrol.py
@@ -162,7 +162,7 @@ async def alivemenu(event):
                 Button.inline("ᴀʟɪᴠᴇ ɴᴀᴍᴇ", data="alvname"),
                 Button.inline("ʙᴀᴄᴋ", data="apiset"),
             ]
-        )
+        ),
 
 
 @callback(data=re.compile(b"inlinemenu"))
