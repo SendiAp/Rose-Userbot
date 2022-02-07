e739a59d95af3a0a805dfe9d1fa80c52e4abd27e
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-07 12:01:47 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -21,29 +21,3 @@ async def startupmessage():
     except Exception as e:
         LOGS.error(e)
         return None
-    try:
-       from userbot.modules.sql_helper.globals import delgvar, gvarstatus
-
-        msg_details = gvarstatus("restartstatus").split("\n")
-        if msg_details:
-            msg_details = msg_details[0]
-    except Exception as e:
-        LOGS.error(e)
-        return None
-    try:
-        if msg_details:
-            await tgbot.check_testcases()
-            message = await tgbot.get_messages(msg_details[0], ids=msg_details[1])
-            text = message.text + "\n\n**Ok Bot is Back and Alive.**"
-            await tgbot.edit_message(msg_details[0], msg_details[1], text)
-            if gvarstatus("restartstatus") is not None:
-                await drgub.send_message(
-                    msg_details[0],
-                    f"{cmd}ping",
-                    reply_to=msg_details[1],
-                    schedule=timedelta(seconds=10),
-                )
-            del_keyword_collectionlist("restart_update")
-    except Exception as e:
-        LOGS.error(e)
-        return None
