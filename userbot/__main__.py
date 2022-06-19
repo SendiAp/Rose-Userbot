630040f5a67d89ebb94df44ec68147b94041572e
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-06-19 20:58:57 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,51 +1,35 @@
-
+# Copyright (C) 2019 The Raphielscape Company LLC.
+# Userbot
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
 """ Userbot start point """
 
 import sys
 from importlib import import_module
 
-import requests
 from telethon.tl.functions.channels import InviteToChannelRequest
-
-from userbot import BOT_TOKEN, BOT_USERNAME, BOT_VER, BOTLOG_CHATID
-from userbot import DEVS, LOGS, bot
+from userbot import ALIVE_NAME, BOTLOG_CHATID, BOT_USERNAME, BOT_VER, LOGS, bot, branch
 from userbot.modules import ALL_MODULES
-from userbot.utils import autobot, checking, startupmessage
+from userbot.utils.tools import checking
 
 try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
     bot.start()
-    user = bot.get_me()
-    blacklistrose = requests.get(
-        "https://raw.githubusercontent.com/SendiAp/Remaining/master/blacklistrose.json"
-    ).json()
-    if user.id in blacklistrose:
-        LOGS.warning(
-            "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOTnya GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nCredits: @pikyus1"
-        )
-        sys.exit(1)
-    if not DEVS:
-        LOGS.warning(
-            f"EOL\nRose-UserBot v{BOT_VER}, Copyright © 2021-2022 RoseUserbot• <https://github.com/SendiAp>"
-        )
-        sys.exit(1)
-except Exception as e:
+    LOGS.info(f"⚡Rose - Userbot⚡ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
-
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
-
-LOGS.info(
-    f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/Rose-Userbot"
-)
-
-LOGS.info(f"Rose-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
 
 
 async def rose_userbot_on():
     try:
         if BOTLOG_CHATID != 0:
-            await startupmessage()
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"💢 Rose - Userbot Berhasil Diaktfikan 💢\n╼┅━━━━━╍━━━━━┅╾\n❍▹ Bot Of : {ALIVE_NAME}\n❍▹ BotVer : {BOT_VER}@{branch}\n╼┅━━━━━╍━━━━━┅╾",
+            )
     except Exception as e:
         LOGS.info(str(e))
     try:
@@ -53,11 +37,8 @@ async def rose_userbot_on():
     except BaseException:
         pass
 
-
-bot.loop.run_until_complete(checking())
 bot.loop.run_until_complete(rose_userbot_on())
-if not BOT_TOKEN:
-    bot.loop.run_until_complete(autobot())
+bot.loop.run_until_complete(checking())
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
